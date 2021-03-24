3f415f4af07b9d538756a58108ad9fc05562c6ba
andrey <captain.destroyer@yandex.ru>
2021-03-24 20:12:22 +0700

    remove logic from template

--- views/Season/index.js
+++ views/Season/index.js
@@ -1,8 +1,11 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import SeasonComponent from './Season';
 
 function Season({ serial, season, series, seria, ...rest }) {
-
+  const [seriaNumber, setSeria] = useState(seria);
+  const router = useRouter();
   const breadcrumbs = [
     {
       title: 'Главная',
@@ -18,7 +21,42 @@ function Season({ serial, season, series, seria, ...rest }) {
     },
   ];
 
-  return <SeasonComponent breadcrumbs={breadcrumbs} series={series} seria={seria} {...rest} />;
+  const getPath = path => {
+    const index = path.indexOf('?');
+
+    if (index === -1) {
+      return path;
+    }
+
+    return path.slice(0, index);
+  };
+
+  if (router.query.seria === undefined && typeof window !== 'undefined') {
+    router.push({
+      pathname: getPath(router.asPath),
+      query: { seria: 1 },
+    });
+  }
+  const activeSeria = series.find(seria => seria.number === +seriaNumber);
+
+  const switchSeria = seria => {
+    setSeria(seria);
+    
+    router.push({
+      pathname: getPath(router.asPath),
+      query: { seria },
+    });
+  };
+
+  return (
+    <SeasonComponent
+      breadcrumbs={breadcrumbs}
+      series={series}
+      seria={activeSeria}
+      switchSeria={switchSeria}
+      {...rest}
+    />
+  );
 }
 
 Season.propTypes = {
